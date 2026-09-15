5b793462b9f0e4cf243712e34ba76bd757f9fb2b
follepier <follepier@gmail.com>
2026-09-15 13:56:50 +0200

    Load footer creator credit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home.jsx";
 import "./pages/ThemeDark.css";
+import "./pages/FooterCredit.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
